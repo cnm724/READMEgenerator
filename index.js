f8bcac4eee82f549164e1b918538b620136374ae
unknown <cnm.coursework@gmail.com>
2024-04-28 15:19:53 -0600

    index.js-working writefile and questions

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
-// TODO: Include packages needed for this application
+// 
+// Packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const fileName = "README.md"
+const fileName = "README.md";
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: "What's your project's name?",
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: "Describe your project?",
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: "How do you install this app?",
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: "Provide a brief description of instructions/examples for use?",
-      },
-      {
-        type: 'input',
-        name: 'credits',
-        message: "Who did you collaborate with on this project?",
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'What license did you use?',
-        choices: ["Apache", "GNU v3.0", "MIT", "BSD 2","BSD 3"],
-        },
-        {
-          type: 'input',
-          name: 'tests',
-          message: "Please write an example to test the app?",
-        },
-        {
-          type: 'input',
-          name: 'github',
-          message: "What is your GitHub Username?",
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: "What is your email?",
-        },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What's your project's title?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Describe your project?",
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: "How do you install this app?",
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: "Provide a brief description of instructions/examples for use?",
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: "Who did you collaborate with on this project?",
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license did you use?',
+    choices: ["Apache", "GNU_v3.0", "MIT", "BSD_2", "BSD_3", "None"],
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: "Please write an example to test the app?",
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is your GitHub Username?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is your email?",
+  },
 
 ];
-inquirer.prompt(questions).then((answers) => {
-  console.log('User Info:');
-  console.log(answers);
-});
 
-// TODO: Create a function to write README file
+// Function that writes a README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
-  err ? console.error(err) : console.log('The file has been written!')
-);
+    err ? console.error(err) : console.log('The file has been written!')
+  );
 }
 
-// TODO: Create a function to initialize app
+//initialize app
 function init() {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      writeToFile(fileName, generateMarkdown(answers));
+    })
+    .catch ((err) => console.error(err));
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,62 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // turn into a turnary
-  if (license) {
-    console.log("print a badge");
-    // ![Static Badge](https://img.shields.io/badge/:badgeContent)
-  } else {
-    console.log("no license used");
+  if (license !== "None") {
+    return `![${license} License](https://img.shields.io/badge/license-${license}-blue)`;
   }
+  return "";
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 // put link on badge????
 function renderLicenseLink(license) {
-
+  if(license !== "None"){
+    return "- [License](#license)";
+}else{
+  return "";
+}
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+    This application is covered under the ${license} license.`
+  }
+  return "";
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ## Description
 
-## Description
-${data.description}
+  ${renderLicenseBadge(data.license)}
 
-## Table of Contents 
+  ${data.description}
 
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license) 
-- [Tests](#Tests)
+  ## Table of Contents 
 
-## Installation
-${data.installation}
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  ${renderLicenseLink(data.license)}
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
 
 
-## Usage 
-${data.usage}
+  ## Usage 
+  ${data.usage}
 
-## Contribution
-* Collaborator(s): ${data.credits}
+  ## Credits
+  * Collaborator(s): ${data.credits}
 
-## Licenses
-This application is covered under the ${data.license} license!
+  ${renderLicenseSection(data.license)}
 
-## Tests
-${data.tests}
+  ## Tests
+  ${data.tests}
 
-## Questions
-You can find me at ${data.github}, and reach me at ${data.email} if you have any additional questions!`
+  ## Questions
+  You can find me at [${data.github}](https://github.com/${data.github}), and reach me at [${data.email}](mailto:${data.email}) if you have any additional questions!`
 
 }
 
 module.exports = generateMarkdown;
-
-// credits and license add to readme if added or not?
